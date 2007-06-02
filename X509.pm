5c76f094f4962e7d6fe7cc11ee3f6a8245b9b496
Dan Sully <daniel-github@electricrain.com>
2007-06-02 18:20:49 +0000

    PAUSE doesn't like my version number.

--- X509.pm
+++ X509.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION @EXPORT_OK);
 use Exporter;
 use base qw(Exporter);
 
-$VERSION = '0.4.1';
+$VERSION = '0.5';
 
 @EXPORT_OK = qw(
 	FORMAT_UNDEF FORMAT_ASN1 FORMAT_TEXT FORMAT_PEM FORMAT_NETSCAPE
