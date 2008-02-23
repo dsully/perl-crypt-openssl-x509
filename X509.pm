fb167b56e54e70c5fe746422939b48ed7a1812af
Dan Sully <daniel-github@electricrain.com>
2008-02-23 22:19:04 +0000

    #28684: Failed test 'use Crypt::OpenSSL::X509;'

--- X509.pm
+++ X509.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION @EXPORT_OK);
 use Exporter;
 use base qw(Exporter);
 
-$VERSION = '0.5';
+$VERSION = '0.6';
 
 @EXPORT_OK = qw(
 	FORMAT_UNDEF FORMAT_ASN1 FORMAT_TEXT FORMAT_PEM FORMAT_NETSCAPE
