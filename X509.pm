66c91093ccdecfb5e60cd8c2501f9a2beb6ca875
Dan Sully <daniel-github@electricrain.com>
2010-08-06 09:19:23 -0700

    Update version.

--- X509.pm
+++ X509.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION @EXPORT_OK);
 use Exporter;
 use base qw(Exporter);
 
-$VERSION = '1.2';
+$VERSION = '1.3';
 
 @EXPORT_OK = qw(
   FORMAT_UNDEF FORMAT_ASN1 FORMAT_TEXT FORMAT_PEM FORMAT_NETSCAPE
