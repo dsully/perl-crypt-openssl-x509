ed8b74842b16ba3c527470a13d6331abaed43eef
Dan Sully <daniel-github@electricrain.com>
2011-04-13 06:24:07 -0700

    Bump version to deal with CPAN madness.

--- X509.pm
+++ X509.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION @EXPORT_OK);
 use Exporter;
 use base qw(Exporter);
 
-$VERSION = '1.7.1';
+$VERSION = '1.8';
 
 @EXPORT_OK = qw(
   FORMAT_UNDEF FORMAT_ASN1 FORMAT_TEXT FORMAT_PEM FORMAT_NETSCAPE
