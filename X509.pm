e077b5c1557e96ddce4719d90b4dc5d8623dbc78
jonasbn <jonasbn@gmail.com>
2024-07-03 20:13:58 +0200

    Version numbers are weird

--- X509.pm
+++ X509.pm
@@ -8,7 +8,7 @@ use base qw(Exporter);
 
 use Convert::ASN1;
 
-use version; our $VERSION = version->declare('1.10.0');
+use version; our $VERSION = version->declare('1.10.1');
 
 our @EXPORT_OK = qw(
   FORMAT_UNDEF FORMAT_ASN1 FORMAT_TEXT FORMAT_PEM
